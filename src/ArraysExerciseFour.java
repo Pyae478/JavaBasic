42d6bb141bc2038c7d2432ece005b2062f2dbb34
unknown <pyaeyati09@gmail.com>
2025-09-20 19:18:16 +0630

    Initial Commit

--- src/ArraysExerciseFour.java
+++ src/ArraysExerciseFour.java
@@ -6,8 +6,8 @@ public class ArraysExerciseFour {
         int[] copyNumbers = new int[6]; // determine copy array for original
        // System.arraycopy(name of original array,start of original index,name of copy array,start of copy array position,length of )
 
-        System.arraycopy(numbers,1,0,6);
-        System.out.println(Arrays.toString(copyNumbers));
+      //  System.arraycopy(numbers,1,0,5);
+       // System.out.println(Arrays.toString(copyNumbers));
 
 
 
